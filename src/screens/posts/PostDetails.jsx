2c0e11828d2dbe97d892368feff16084ab5fcb2c
kyle g <kyleguerrero85@gmail.com>
2021-09-19 03:55:24 -0500

    chore: reorganized screens and scaffolding

---
+++ src/screens/posts/PostDetails.jsx
@@ -0,0 +1,10 @@
+
+function PostDetails() {
+  return (
+    <div>
+      <h2>Post Details</h2>
+    </div>
+  )
+}
+
+export default PostDetails;
---
+++ src/screens/posts/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Toolbar from "@components/Toolbar"
+import Cards from "@components/Cards"
+function Home() {
+
+  return (
+    <React.Fragment>
+      <Toolbar />
+      <Cards />
+    </React.Fragment>
+  )
+}
+
+export default Home;
