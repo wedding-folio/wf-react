30940c446a65f58bce894d64ffe0c46aca617d55
kyle g <kyleguerrero85@gmail.com>
2021-09-20 02:31:20 -0500

    feat: setup container for image, text and image wrapper

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,20 @@
 
+import "./styles.css"
 function Details() {
   return (
-    <div>
-      <h2>Post Details</h2>
-    </div>
+    <section className="details-container">
+
+      <div className="details-wrapper">
+        <div className="detailsimg-container">
+          <img src="https://source.unsplash.com/featured/1000x600?wedding" alt="A picture from a wedding" />
+        </div>
+        {/* <div className="bg-img"></div> */}
+      </div>
+
+      <div className="details-wrapper">
+        <h2>Some Text</h2>
+      </div>
+    </section>
   )
 }
 
